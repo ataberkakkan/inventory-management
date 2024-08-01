3854d760e5bd346942b78d8c9ce128062816da55
ataberkakkan <akkanataberk@gmail.com>
2024-08-01 12:38:53 +0300

    Sidebar Draft and Redux Installation

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,9 +1,13 @@
-import Navbar from "@/app/(components)/Navbar";
+"use client";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider from "./redux";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar
+      <Sidebar />
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
       >
@@ -13,4 +17,13 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     </div>
   );
 };
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
+
 export default DashboardWrapper;
